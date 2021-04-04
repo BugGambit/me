9be16174244ac98e6369e6c94d9789dd27baf5e6
BugGambit <fredrik.anfinsen@gmail.com>
2021-04-04 19:16:40 +0200

    fix: ensure correct path for static files

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import styled from 'styled-components';
+import staticFileUrl from 'utils/staticFileUrl';
 
 import ProjectCard from './ProjectCard';
 
@@ -16,19 +17,27 @@ function Projects(): JSX.Element {
       </div>
       <Grid alignItems="stretch" container direction="row"
 justify="center" spacing={0} style={{ position: 'relative' }}>
-        <ProjectCard href="https://github.com/BugGambit/mandelbrot" imageSrc="/mandelbrot.gif" title="Mandelbrot">
+        <ProjectCard href="https://github.com/BugGambit/mandelbrot" imageSrc={staticFileUrl('/mandelbrot.gif')} title="Mandelbrot">
           Superfast Mandelbrot fractal in browser using WebGL.
         </ProjectCard>
-        <ProjectCard href="https://github.com/BugGambit/ball-sort-puzzle-solver" imageSrc="/ball-sort.gif" title="Ball sort puzzle solver">
+        <ProjectCard
+          href="https://github.com/BugGambit/ball-sort-puzzle-solver"
+          imageSrc={staticFileUrl('/ball-sort.gif')}
+          title="Ball sort puzzle solver"
+        >
           Superfast Mandelbrot fractal in browser using WebGL.
         </ProjectCard>
-        <ProjectCard href="https://github.com/BugGambit/snake" imageSrc="/snake.gif" title="Snake in Rust">
+        <ProjectCard href="https://github.com/BugGambit/snake" imageSrc={staticFileUrl('/snake.gif')} title="Snake in Rust">
           The classical game, Snake, written in Rust.
         </ProjectCard>
-        <ProjectCard href="https://github.com/BugGambit/weight4it" imageSrc="/weight4it.gif" title="weight4it">
+        <ProjectCard href="https://github.com/BugGambit/weight4it" imageSrc={staticFileUrl('/weight4it.gif')} title="weight4it">
           Weight tracking app.
         </ProjectCard>
-        <ProjectCard href="/algoritmer_datastrukturer.pdf" imageSrc="/algo-book.gif" title="Book: Algorithms and data structures">
+        <ProjectCard
+          href={staticFileUrl('/algoritmer_datastrukturer.pdf')}
+          imageSrc={staticFileUrl('/algo-book.gif')}
+          title="Book: Algorithms and data structures"
+        >
           <Typography color="textSecondary" component="p" variant="body2">
             In my last year in high school, I wrote this Norwegian book to help students do well in computer science competitions.
           </Typography>
